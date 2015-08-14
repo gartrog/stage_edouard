a98bb079a69227fa01c1c1256d54034777c40f90
Nicolas Morange <nicolas.morange@cern.ch>
2015-08-14 15:45:28 +0200

    R20 ttbar sample

--- run.cxx
+++ run.cxx
@@ -8,10 +8,12 @@
 
 int main(int argc, char** argv) {
   //File f("smallttbar.root");
-  File f("ttbar.root");
+  //File f("ttbar.root");
+  File f("ttbarR20.root");
   //File f("ZHvv125.root");
   TTree* tree = f.getObject<TTree>("tupletest");
-  VHAnalysis analysis(tree, "output.root");
+  //VHAnalysis analysis(tree, "output.root");
+  VHAnalysis analysis(tree, "outputR20.root");
   //VHAnalysis analysis(tree, "outputHiggs.root");
   analysis.Loop();
   analysis.WriteHistos();
@@ -19,21 +21,21 @@ int main(int argc, char** argv) {
   f->Close();
   std::cout << "Closing input file" << std::endl;
 
-  // std::vector<std::string> names {"btag_nJet2MET0_jet1truthvsjet2truth", "btag_nJet2MET1_jet1truthvsjet2truth",
-                                  // "btag_nJet2MET2_jet1truthvsjet2truth", "btag_nJet2MET3_jet1truthvsjet2truth",
-                                  // "btag_nJet3MET0_jet1truthvsjet2truth", "btag_nJet3MET1_jet1truthvsjet2truth",
-                                  // "btag_nJet3MET2_jet1truthvsjet2truth", "btag_nJet3MET3_jet1truthvsjet2truth"};
-  // for(std::string& s : names) {
-    // JetsFlavor(s);
-  // }
-  // 
-  // std::vector<std::string> noms {"nJet2MET0_compo", "nJet2MET1_compo",
-                                  // "nJet2MET2_compo", "nJet2MET3_compo",
-                                  // "nJet3MET0_compo", "nJet3MET1_compo",
-                                  // "nJet3MET2_compo", "nJet3MET3_compo"};
-  // for(std::string& ss : noms) {
-    // Sorting(ss);
-  // }
-  // 
+  std::vector<std::string> names {"btag_nJet2MET0_jet1truthvsjet2truth", "btag_nJet2MET1_jet1truthvsjet2truth",
+    "btag_nJet2MET2_jet1truthvsjet2truth", "btag_nJet2MET3_jet1truthvsjet2truth",
+    "btag_nJet3MET0_jet1truthvsjet2truth", "btag_nJet3MET1_jet1truthvsjet2truth",
+    "btag_nJet3MET2_jet1truthvsjet2truth", "btag_nJet3MET3_jet1truthvsjet2truth"};
+  for(std::string& s : names) {
+    JetsFlavor(s);
+  }
+
+  std::vector<std::string> noms {"nJet2MET0_compo", "nJet2MET1_compo",
+    "nJet2MET2_compo", "nJet2MET3_compo",
+    "nJet3MET0_compo", "nJet3MET1_compo",
+    "nJet3MET2_compo", "nJet3MET3_compo"};
+  for(std::string& ss : noms) {
+    Sorting(ss);
+  }
+
   return 0;
 }
